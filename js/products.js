5fffe229181d3c68bed693577358adb6bd9574eb
Akhila <murasaniakhila1234@gmail.com>
2026-07-20 14:27:35 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,41 +1,48 @@
-const products = [
-  {
-    id: 1,
-    name: "Laptop",
-    price: 50000,
-    image: "image/laptop.png"
-  },
-  {
-    id: 2,
-    name: "Mobile",
-    price: 20000,
-    image: "image/mobile.png"
-  },
-  {
-    id: 3,
-    name: "SmartWatch",
-    price:4000,
-    image: "image/headphones.png"
-  },
-  {
-    id: 4,
-    name: "Headphones",
-    price: 3000,
-    image: "image/headphones.png"
-  }
-];
+const API = "http://localhost:5001/api/products";
 
-const container = document.getElementById("products");
+fetch(API)
 
-products.forEach(product => {
-    container.innerHTML += `
+.then(res => res.json())
+
+.then(products => {
+
+    const container = document.getElementById("products");
+
+    if(!container) return;
+
+    products.forEach(product => {
+
+        container.innerHTML += `
+
         <div class="card">
-            <img src="${product.image}" width="200">
+
+            <img src="image/${product.image}" width="200">
+
             <h3>${product.name}</h3>
+
             <p>₹${product.price}</p>
+
             <button onclick="addToCart(${product.id})">
                 Add To Cart
             </button>
+
         </div>
-    `;
+
+        `;
+
+    });
+
 });
+
+
+function addToCart(id){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push(id);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Added to Cart");
+
+}
